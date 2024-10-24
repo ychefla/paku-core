f1fba0497b679d020db74ae0e380d3d21c6d392a
Jossu <johannes.kokkila@gmail.com>
2024-10-24 23:32:45 +0300

    Refactor main loop to handle WiFi and MQTT connections, update time, process flow data, and send data to MQTT
    - Added logic to check and maintain WiFi connection.
    - Added logic to check and maintain MQTT connection.
    - Updated time using the time client.
    - Placeholder for updating heater status.
    - Updated intervals based on heater status.
    - Processed flow data.
    - Sent data to the MQTT broker.
    - Added goToSleep function to put the device to sleep after 30 seconds of activity.
    - Added TODO file

--- paku_core/src/main.cpp
+++ paku_core/src/main.cpp
@@ -6,15 +6,52 @@
 #include "BLEDevice.h"
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "Arduino.h"
+#include "TFT_eSPI.h" /* Please use the TFT library provided in the library. */
+#include "img_logo.h"
+#include "pin_config.h"
+
+/* The product now has two screens, and the initialization code needs a small change in the new version. The LCD_MODULE_CMD_1 is used to define the
+ * switch macro. */
+#define LCD_MODULE_CMD_1
+
+TFT_eSPI tft = TFT_eSPI();
+#define TFT_UPDATE_WAIT 1000
+unsigned long targetTime = 0; // Used for testing draw times
+
+#if defined(LCD_MODULE_CMD_1)
+typedef struct {
+    uint8_t cmd;
+    uint8_t data[14];
+    uint8_t len;
+} lcd_cmd_t;
+
+lcd_cmd_t lcd_st7789v[] = {
+    {0x11, {0}, 0 | 0x80},
+    {0x3A, {0X05}, 1},
+    {0xB2, {0X0B, 0X0B, 0X00, 0X33, 0X33}, 5},
+    {0xB7, {0X75}, 1},
+    {0xBB, {0X28}, 1},
+    {0xC0, {0X2C}, 1},
+    {0xC2, {0X01}, 1},
+    {0xC3, {0X1F}, 1},
+    {0xC6, {0X13}, 1},
+    {0xD0, {0XA7}, 1},
+    {0xD0, {0XA4, 0XA1}, 2},
+    {0xD6, {0XA1}, 1},
+    {0xE0, {0XF0, 0X05, 0X0A, 0X06, 0X06, 0X03, 0X2B, 0X32, 0X43, 0X36, 0X11, 0X10, 0X2B, 0X32}, 14},
+    {0xE1, {0XF0, 0X08, 0X0C, 0X0B, 0X09, 0X24, 0X2B, 0X22, 0X43, 0X38, 0X15, 0X16, 0X2F, 0X37}, 14},
+};
+#endif
 
 // BT settings
 bool scanBT_enabled = true;
 
 // WiFi settings
-const char* wifi_ssid[] = {"iPhone", "JosPar_2.4G", "JosPar_paku"};
-const char* wifi_password[] = {"1234567890", "V11vuskaBaabuska", "V11vuskaBaabuska"};
-const char* mqtt_server = "static.107.192.27.37.clients.your-server.de";
-const int mqtt_port = 1883;
+const char* wifi_ssid[] = {WIFI_SSID_HOME, WIFI_SSID_IPHONE, WIFI_SSID_PAKU};
+const char* wifi_password[] = {WIFI_PASSWORD_HOME, WIFI_PASSWORD_IPHONE, WIFI_PASSWORD_PAKU};
+const char* mqtt_server = MQTT_SERVER;
+const int mqtt_port = MQTT_PORT;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -25,7 +62,7 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);  // GMT, update interval
 
 unsigned long lastTime_sensor = 0;
 unsigned long lastTime_mqtt = 0;
-unsigned long mqttFastInterval = 5000;  // 1 minute interval in ms
+unsigned long mqttFastInterval = 10000;  // 10 second interval in ms
 unsigned long mqttSlowInterval = 3600000;  // 1 hour interval in ms
 unsigned long sensorFastInterval = 5000;  // 5 second interval in ms
 unsigned long sensorSlowInterval = 60000;  // 1 minute interval in ms
@@ -56,8 +93,10 @@ void connectMQTT();
 void createPayload(String topic, float value, String timestamp);
 void connect_wifi();
 void sendToMQTT();
-void processFlowData();
+void processData();
 void scanBT(void* parameter);
+void goToSleep();
+void updateDisplay();
 
 void IRAM_ATTR countRisingEdges() {
   count++;
@@ -79,6 +118,36 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Starting setup...");
     
+    pinMode(PIN_POWER_ON, OUTPUT);
+    digitalWrite(PIN_POWER_ON, HIGH);
+    tft.begin();
+
+#if defined(LCD_MODULE_CMD_1)
+    for (uint8_t i = 0; i < (sizeof(lcd_st7789v) / sizeof(lcd_cmd_t)); i++) {
+        tft.writecommand(lcd_st7789v[i].cmd);
+        for (int j = 0; j < (lcd_st7789v[i].len & 0x7f); j++) {
+            tft.writedata(lcd_st7789v[i].data[j]);
+        }
+
+        if (lcd_st7789v[i].len & 0x80) {
+            delay(120);
+        }
+    }
+#endif
+
+    tft.setRotation(3);
+    tft.setSwapBytes(true);
+    tft.pushImage(0, 0, 320, 170, (uint16_t *)img_logo);
+    delay(2000);
+
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5,0,0)
+    ledcSetup(0, 2000, 8);
+    ledcAttachPin(PIN_LCD_BL, 0);
+    ledcWrite(0, 255);
+#else
+    ledcAttach(PIN_LCD_BL, 200, 8);
+    ledcWrite(PIN_LCD_BL, 255);
+#endif
 
     Serial.println("Setup Wifi Connection...");
     WiFi.mode(WIFI_STA);
@@ -158,28 +227,62 @@ void setup() {
  * - Sends data to the MQTT broker.
  */
 void loop() {
+  // Update heater status here
+  // heaterStatus = ...;  // Retrieve the actual heater status
+  // Update intervals based on heater status
+  updateIntervals();
+  processData();
 
   if (WiFi.status() != WL_CONNECTED) {
     connect_wifi();
   }
-
   if (!client.connected()) {
-    connectMQTT();
+      connectMQTT();
   }
 
+  updateDisplay(); 
   client.loop();
   timeClient.update();
-
-  // Update heater status here
-  // heaterStatus = ...;  // Retrieve the actual heater status
-
-  // Update intervals based on heater status
-  updateIntervals();
-  
-  processFlowData();
-
   sendToMQTT();
 
+  
+  goToSleep();
+
+}
+
+void goToSleep() {
+    // Check if the device has been awake for 30 seconds
+  static unsigned long awakeStartTime = millis();
+  if (millis() - awakeStartTime >= 30000) {
+    Serial.println("Going to sleep for 15 seconds...");
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextSize(2);
+    tft.println("Going to sleep for 15 seconds...");
+    delay(2000);
+    // Configure the ESP32 to wake up after 15 seconds
+    esp_sleep_enable_timer_wakeup(15 * 1000000);
+    esp_deep_sleep_start();
+  } else {
+    delay(1000);
+  }
+}
+
+void updateDisplay() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastTime_sensor >= TFT_UPDATE_WAIT) {
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextSize(3);
+    tft.println("Paku Core");
+    tft.setTextSize(2);
+    tft.println("Time: " + timeClient.getFormattedTime());
+    tft.println("Flow Rate: " + String(flowRate) + " L/min");
+    tft.println("Required Delta T: " + String(requiredDeltaT) + " C");
+    tft.println("Heater Status: " + String(heaterStatus));
+  }
 }
 
 /**
@@ -204,7 +307,7 @@ void loop() {
  * `Serial.print()`, `detachInterrupt()`, `attachInterrupt()`, `random()`, `timeClient.getFormattedTime()`, 
  * and `createPayload()`.
  */
-void processFlowData() {
+void processData() {
   unsigned long currentTime = millis();
   if (currentTime - lastTime_sensor >= sensorInterval) {
     Serial.print(".");
@@ -311,17 +414,25 @@ void sendToMQTT() {
 void connect_wifi() {
   delay(10);
   Serial.println();
-  
-  while (WiFi.status() != WL_CONNECTED){
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(2);
+  tft.println("Connecting to WiFi...");
+
+  while (WiFi.status() != WL_CONNECTED) {
     for (int i = 0; i < sizeof(wifi_ssid)/sizeof(wifi_ssid[0]); i++) {
       Serial.print("Connecting to ");
       Serial.println(wifi_ssid[i]);
+      tft.println("Connecting to " + String(wifi_ssid[i]));
+
       WiFi.begin(wifi_ssid[i], wifi_password[i]);
 
       int attempts = 0;
       while (WiFi.status() != WL_CONNECTED && attempts < 20) {
         delay(500);
         Serial.print(".");
+        tft.print(".");
         attempts++;
       }
 
@@ -330,11 +441,19 @@ void connect_wifi() {
         Serial.println("WiFi connected");
         Serial.println("IP address: ");
         Serial.println(WiFi.localIP());
+
+        tft.println("");
+        tft.println("WiFi connected");
+        tft.println("IP address: ");
+        tft.println(WiFi.localIP().toString());
         return;
       } else {
         Serial.println("");
         Serial.println("Failed to connect to ");
         Serial.println(wifi_ssid[i]);
+
+        tft.println("");
+        tft.println("Failed to connect to " + String(wifi_ssid[i]));
       }
     }
   }
@@ -352,14 +471,23 @@ void connect_wifi() {
 void connectMQTT() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.setTextSize(2);
+    tft.println("Attempting MQTT connection...");
+
     if (client.connect("ESP32Client")) {  // Use a unique client ID for ESP32
-      Serial.println("connected");
+      Serial.println("MQTT connected and subscribed to 'paku/control'");
+      tft.println("MQTT connected and subscribed to 'paku/control'");
       client.subscribe("paku/control");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
+      tft.print("failed, rc=");
+      tft.print(client.state());
+      tft.println(" try again in 5 seconds");
       delay(5000);
     }
   }
@@ -436,11 +564,51 @@ void createPayload(String topic, float value, String timestamp) {
  * the intervals are set to fast intervals. Otherwise, they are set to slow intervals.
  */
 void updateIntervals() {
+  static unsigned long heaterOnStartTime = 0;
+
   if (heaterStatus == 1) {
-    mqttInterval = mqttFastInterval;
-    sensorInterval = sensorFastInterval;
+    if (heaterOnStartTime == 0) {
+      heaterOnStartTime = millis();
+    }
+
+    if (millis() - heaterOnStartTime >= 3600000) { // 1 hour in milliseconds
+      mqttInterval = mqttSlowInterval;
+      sensorInterval = sensorSlowInterval;
+    } else {
+      mqttInterval = mqttFastInterval;
+      sensorInterval = sensorFastInterval;
+    }
   } else {
+    heaterOnStartTime = 0;
     mqttInterval = mqttSlowInterval;
     sensorInterval = sensorSlowInterval;
   }
 }
+
+// TFT Pin check
+#if PIN_LCD_WR  != TFT_WR || \
+    PIN_LCD_RD  != TFT_RD || \
+    PIN_LCD_CS    != TFT_CS   || \
+    PIN_LCD_DC    != TFT_DC   || \
+    PIN_LCD_RES   != TFT_RST  || \
+    PIN_LCD_D0   != TFT_D0  || \
+    PIN_LCD_D1   != TFT_D1  || \
+    PIN_LCD_D2   != TFT_D2  || \
+    PIN_LCD_D3   != TFT_D3  || \
+    PIN_LCD_D4   != TFT_D4  || \
+    PIN_LCD_D5   != TFT_D5  || \
+    PIN_LCD_D6   != TFT_D6  || \
+    PIN_LCD_D7   != TFT_D7  || \
+    PIN_LCD_BL   != TFT_BL  || \
+    TFT_BACKLIGHT_ON   != HIGH  || \
+    170   != TFT_WIDTH  || \
+    320   != TFT_HEIGHT
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#endif
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5,0,0)
+#error  "The current version is not supported for the time being, please use a version below Arduino ESP32 3.0"
+#endif
