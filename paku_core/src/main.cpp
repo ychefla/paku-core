789abc292994db29d0bdcd71ee31d08a6334e42c
Johannes Kokkila <>
2025-12-26 02:59:32 +0200

    feat: Add device ID to published device status

--- paku_core/src/main.cpp
+++ paku_core/src/main.cpp
@@ -1788,6 +1788,7 @@ void publishDeviceStatus() {
            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   
   doc["online"] = true;
+  doc["device_id"] = deviceId;
   doc["mac_address"] = macStr;
   
   // Add friendly name if configured
